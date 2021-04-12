7a6b203433259612b62235d1bb3680fb9a3c13b3
Steeve McCauley <steeve@oneguycoding.com>
2021-04-12 17:55:18 -0400

    Initial scripts for git tags and pack

---
+++ bin/git_tag.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ME=$(basename $0)
+MD=$(cd $(dirname $0); pwd)
+
+test=""
+
+info() {
+	echo -e "${test}> $*"
+}
+
+[ $# -lt 2 ] && test="DRYRUN" && info -e "set > 1 parameters to create tag"
+
+info $MD
+cd $MD/../clippie@blackjackshellac.ca
+[ $? -ne 0 ] && echo "Failed to change to extension directory" && exit 1
+
+info Working in $(pwd)
+
+gsv=$(gnome-shell --version | cut -f1,2 -d'.' | sed 's/[a-z ]//gi')
+gsv=$(cat metadata.json | ruby -rjson -e 'puts JSON.parse(STDIN.read)["shell-version"].join("_").gsub(/[.]/,"_")')
+ver=$(cat metadata.json | ruby -rjson -e 'puts JSON.parse(STDIN.read)["version"]')
+ver=$(echo -n $ver)
+
+msg="Clippie ver$ver for Gnome Shell $gsv"
+tag_name=$(echo -n $msg | sed 's/[ \.]/_/g')
+info git tag -a $tag_name -m "$msg"
+
+[ -z "$test" ] && git tag -a $tag_name -m "$msg"
+
+info Tags ...
+git tag
---
+++ bin/pack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ed=clippie@blackjackshellac.ca
+[ ! -d $ed ] && echo "Extension dir $ed not found" && exit 1
+cd $ed
+
+#clippie-blackjackshellac
+extra_source=$(ls -1 *.js | grep -v prefs.js | grep -v extension.js)
+extra_source="$extra_source $(ls *.ogg *.ui)"
+extra_source="$extra_source"
+echo $extra_source
+eso=""
+for es in $extra_source; do
+	echo "Adding extra $es"
+	eso="$eso --extra-source=$es"
+done
+
+eso="$eso --extra-source=./icons/ --extra-source=./bin/"
+
+cmd="gnome-extensions pack --podir=po/ --schema=schemas/org.gnome.shell.extensions.clippie-blackjackshellac.gschema.xml --gettext-domain=clippie-blackjackshellac $eso -o ../ --force"
+echo $cmd
+$cmd
